a51c02337f3cfe06414ab315ca67f7262026d259
hfLiuX <11685497+hfliu15709732@user.noreply.gitee.com>
2023-10-11 13:04:25 +0800

    feat:B端修改商家营业状态完成，没测试 c端也尚未写

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -8,6 +8,8 @@ import com.sky.enumeration.OperationType;
 import com.sky.vo.DishVO;
 import org.apache.ibatis.annotations.*;
 
+import java.util.List;
+
 @Mapper
 public interface DishMapper {
 
@@ -32,4 +34,16 @@ public interface DishMapper {
     void deleteById(Long id);
 
     void update(Dish dish);
+
+    @Select("select a.* from dish a left join setmeal_dish b " +
+            "on a.id = b.dish_id where b.setmeal_id = #{setmealId}")
+    List<Dish> getBySetmealId(Long setmealId);
+
+    /**
+     * 动态条件查询菜品
+     *
+     * @param dish
+     * @return
+     */
+    List<Dish> list(Dish dish);
 }
